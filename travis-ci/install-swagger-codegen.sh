d1d3b15a5585be80f5e75437034ffe8abe435559
Yang Fan <yang@squareup.com>
2017-02-08 17:35:28 -0800

    Add travis CI integration.

---
+++ travis-ci/install-swagger-codegen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SWAGGER_DIR=./swagger-codegen
+
+# download from swagger-codegen from github
+git clone https://github.com/swagger-api/swagger-codegen.git $SWAGGER_DIR
+cd $SWAGGER_DIR
+if [ -n "${SWAGGER_CODEGEN_SHA+1}" ]
+then
+    echo "Using Swagger version \`${SWAGGER_CODEGEN_SHA}\`."
+    git checkout $SWAGGER_CODEGEN_SHA
+else
+    echo "Using lastest Swagger verion."
+fi
+
+# build
+echo "Building swagger-codegen cli..."
+mvn -q clean package
+
+# set $SWAGGER_CMD
+export SWAGGER_CMD="java -jar $SWAGGER_DIR/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar"
+
+cd ..
+
