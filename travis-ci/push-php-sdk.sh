fadbf7a74547d7eced2ecf4413efa224f5fad746
Yang Fan <yang@squareup.com>
2017-02-14 16:36:46 -0800

    Update the ruby sdk templates, add travis for ruby.

--- travis-ci/push-php-sdk.sh
+++ travis-ci/push-php-sdk.sh
@@ -8,28 +8,30 @@ BRANCH_NAME=release/$packageVersion
 
 echo "Going to update PHP SDK..."
 
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 $DIR/php-repo.pem # this key should have push access
+ssh-add $DIR/php-repo.pem
+
+git clone git@github.com:square/connect-php-sdk.git
+cd connect-php-sdk
+git branch $BRANCH_NAME
+git checkout $BRANCH_NAME
+
+echo "Copying files..."
+rm -rf docs lib
+cp -r ../swagger-out/php/SquareConnect/docs .
+cp -r ../swagger-out/php/SquareConnect/lib .
+cp ../swagger-out/php/SquareConnect/.travis.yml .
+cp ../swagger-out/php/SquareConnect/autoload.php .
+cp ../swagger-out/php/SquareConnect/composer.json .
+cp ../swagger-out/php/SquareConnect/README.md .
+
+git add .
+git commit -m "Pushed by Travis CI from connect-api-specification. Commit: ${TRAVIS_COMMIT}"
+
 # only push to sdk repo when it's merged into master
 if [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "" -a "${TRAVIS_BRANCH}" = "master" ];
 then
-    eval "$(ssh-agent -s)" #start the ssh agent
-    chmod 600 $DIR/php-repo.pem # this key should have push access
-    ssh-add $DIR/php-repo.pem
-
-    git clone git@github.com:square/connect-php-sdk.git
-    cd connect-php-sdk
-    git checkout $BRANCH_NAME
-
-    echo "Copying files..."
-    rm -rf docs lib
-    cp -r ../swagger-out/php/SquareConnect/docs .
-    cp -r ../swagger-out/php/SquareConnect/lib .
-    cp -r ../swagger-out/php/SquareConnect/.travis.yml .
-    cp -r ../swagger-out/php/SquareConnect/autoload.php .
-    cp -r ../swagger-out/php/SquareConnect/composer.json .
-    cp -r ../swagger-out/php/SquareConnect/README.md .
-
-    git add .
-    git commit -m "Pushed by Travis CI from connect-api-specification. Commit: ${TRAVIS_COMMIT}"
     git push -u origin $BRANCH_NAME
 else
     echo "Skip pull request."
