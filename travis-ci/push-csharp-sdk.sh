a9677a3600fa2041b82121618b2de35da7eddce7
Francisco Rojas <frojas@alumnos.utalca.cl>
2017-06-21 17:59:37 -0700

    Release 2.2.0 (#58)
    * When changes are merged to master, push them to the SDK master branch.
    
    * update the csharp template
    
    * Update OpenAPI spec file with new Checkout featues
    
    Additionally, this contains a breaking change. The
    CreateOrderRequestOrder object is removed in favor of
    CreateOrderRequest.
    
    * Update packageVersion to 2.2.0
    
    * Rename DiscountType and TaxType
    
    * Update Specification file with lastest changes
    
    * update ORDERS_READ and ORDERS_WRITE description

--- travis-ci/push-csharp-sdk.sh
+++ travis-ci/push-csharp-sdk.sh
@@ -15,18 +15,11 @@ ssh-add $DIR/csharp-repo.pem
 git clone git@github.com:square/connect-csharp-sdk.git
 cd connect-csharp-sdk
 
-RELEASE_BRANCH=release/$packageVersion
-if [ `git branch -r | grep "${RELEASE_BRANCH}"` ];
-then
-    git checkout $RELEASE_BRANCH
-else
-    git checkout -b $RELEASE_BRANCH
-    git push -u origin $RELEASE_BRANCH
-fi
+git checkout master
 
 if [ "${TRAVIS_BRANCH}" = "master" ];
 then
-    BRANCH_NAME=$RELEASE_BRANCH
+    BRANCH_NAME=master
 else
     BRANCH_NAME=travis-ci/$TRAVIS_BRANCH
     if [ `git branch -r | grep "${BRANCH_NAME}"` ];
