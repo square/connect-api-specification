1dfde01f69dc264fcae9b45340134f4e7ee54177
Juan Pablo Marzetti <jpm@squareup.com>
2017-06-28 15:55:51 -0700

    Add Link header extractor on PHP code

--- travis-ci/install-swagger-codegen.sh
+++ travis-ci/install-swagger-codegen.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SWAGGER_DIR=./swagger-codegen
+SWAGGER_PATCHES_DIR=`pwd`/travis-ci/swagger-codegen-patches
 
 # download from swagger-codegen from github
 git clone https://github.com/swagger-api/swagger-codegen.git $SWAGGER_DIR
@@ -8,7 +9,13 @@ cd $SWAGGER_DIR
 if [ -n "${SWAGGER_CODEGEN_SHA+1}" ]
 then
     echo "Using Swagger version \`${SWAGGER_CODEGEN_SHA}\`."
-    git checkout $SWAGGER_CODEGEN_SHA
+    git checkout -f $SWAGGER_CODEGEN_SHA
+    SWAGGER_CODEGEN_PATCH_FILE="$SWAGGER_PATCHES_DIR/$SWAGGER_CODEGEN_SHA.patch"
+    if [ -f $SWAGGER_CODEGEN_PATCH_FILE ]
+    then
+        echo "Applying patch $SWAGGER_CODEGEN_PATCH_FILE"
+        git apply $SWAGGER_CODEGEN_PATCH_FILE
+    fi
 else
     echo "Using lastest Swagger verion."
 fi
